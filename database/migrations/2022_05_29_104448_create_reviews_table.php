0fe30483a351ab7484c9a0166e6be0e5aff7270e
Saqlainkhadim <94662735+saqlainkhadimbusiness@users.noreply.github.com>
2022-05-29 05:15:43 -0700

    MODEl  and migration of reviews

---
+++ database/migrations/2022_05_29_104448_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('review_id');
+            $table->string('person_name');
+            $table->string('position_in_company');
+            $table->string('company_name');
+            $table->enum('rating',[1,2,3,4,5]);
+            $table->string('review_message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
