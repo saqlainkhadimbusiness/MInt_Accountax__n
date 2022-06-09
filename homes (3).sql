68212662589fb25a3c55a98dfd9fec8f408978a6
Saqlainkhadim <94662735+saqlainkhadimbusiness@users.noreply.github.com>
2022-06-09 16:13:27 -0700

    homes DB export  file

---
+++ homes (3).sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 10, 2022 at 01:12 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.1.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mintpoda_mint`
+--
+
+--
+-- Dumping data for table `homes`
+--
+
+INSERT INTO `homes` (`id`, `meta_title`, `meta_title_desc`, `slider_text_1`, `slider_text_2`, `slider_text_3`, `services_heading`, `services_desc`, `services_icon_1`, `services_heading_1`, `services_slug_1`, `services_desc_1`, `services_icon_2`, `services_heading_2`, `services_slug_2`, `services_desc_2`, `services_icon_3`, `services_heading_3`, `services_slug_3`, `services_desc_3`, `services_icon_4`, `services_heading_4`, `services_slug_4`, `services_desc_4`, `services_icon_5`, `services_heading_5`, `services_slug_5`, `services_desc_5`, `services_icon_6`, `services_heading_6`, `services_slug_6`, `services_desc_6`, `company_heading`, `company_heading_desc`, `company_image`, `company_image_alt`, `company_desc`, `company_item_1`, `company_item_per_1`, `company_item_2`, `company_item_per_2`, `success_heading`, `success_heading_desc`, `success_head_1`, `success_head_desc_1`, `success_head_2`, `success_head_desc_2`, `success_head_3`, `success_head_desc_3`, `success_head_4`, `success_head_desc_4`, `success_head_5`, `success_head_desc_5`, `success_head_6`, `success_head_desc_6`, `created_at`, `updated_at`) VALUES
+(1, 'Welcome to Mint Accountax for Online Cheap Accountants UK', 'MintAccountax providing online accountants, Tax, and other legal & financial services at affordable prices. Hurry Up, save your taxes now and grow your business in UK.', '<h1>Professional Accounting<br />\r\n&amp; Taxation Services</h1>\r\n\r\n<p>Mint Accountax is a leading comapny providing high quality accounting &amp; tax services in UK.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>', '<h1>Quality Tax Help for Your Business</h1>\r\n\r\n<p>Our team is ready to offer you consulting &amp; any necessary help with your company&rsquo;s taxation.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>', '<h1>Helping You Manage Your Business Finance</h1>\r\n\r\n<p>Since its establishment, Mint Accountax provides accounting assistance to companies of any size all over the UK.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>&nbsp;</p>', 'Our Services', 'Since the foundation of Mint Accountax, we have been the number 01 tax counseling and accounting company. A\r\n            huge variety of services and solutions we offer to our clients that are fit according to their business\r\n            administration.', 'linearicons-book2', 'Bookkeeping', 'bookkeeping-services', 'Your accounts are handled and you are provided financial details.', 'linearicons-bubbles', 'Annual Accounts', 'annual-accounts', 'We are eager to offer clients recognized profitable opportunities by avoiding costly problems.', 'linearicons-bubble-text', 'Small Business Consultation', 'business-consulting-services', 'We help your business to grow on its potential growth', 'linearicons-document', 'Payroll Services', 'payroll-services', 'A personalized payroll system is developed for the convenience of the clients.', 'linearicons-calculator', 'Tax Preparation & Consultation', NULL, 'Get your personal and business tax returns from the professional and licensed tax preparers.', 'linearicons-cog', 'Other Services', NULL, 'For a variety of other financial advisory assistance join our team today.', 'A Few Words About Our Company', 'Mint Accountax was originated in 2019 and introducing you to the next level of accounting assistances and other advisories related to the financial system. Our main aim is the success of our clients including both individual and company.', 'home/oLKAipEPqQFdHamHf6heQhlN4jCVmJVKK5zCb2ws.jpeg', 'a lady with black  coat', 'The value-based personal services are offering by us with almost all the proficiency for which a client expects from an accounting firm. We work with clients on all their personal and business aspects or portfolios. The business is precise and tenacious in meeting the prompt needs of customers and exercise premonition in anticipating any future assessment or other finance-related ramifications.', 'Accounting', '75', 'Tax Advisory', '50', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-06-10 06:02:57');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
