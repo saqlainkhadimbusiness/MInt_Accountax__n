fe5a35ef15bd924895cf0019630b0bc8d2f0ec6c
Saqlainkhadim <94662735+saqlainkhadimbusiness@users.noreply.github.com>
2022-06-09 11:40:33 -0700

    homes editable table

--- app/home.php
+++ app/home.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class home extends Model
 {
-    //
+
+    protected  $fillable=['id','home_title','group_id','home_title_desc','slider_text_1','slider_text_2','slider_text_3','services_heading','services_desc','services_icon_1','services_heading_1','services_desc_1','services_icon_2','services_heading_2','services_desc_2','services_icon_3','services_heading_3','services_desc_3','services_icon_4','services_heading_4','services_desc_4','services_icon_5','services_heading_5','services_desc_5','services_icon_6','services_heading_6','services_desc_6','company_heading','company_heading_desc','company_image','company_desc','company_item_1','company_item_per_1','company_item_2','company_item_per_2','success_heading','success_heading_desc','success_head_1','success_head_desc_1','success_head_2','success_head_desc_2','success_head_3','success_head_desc_3','success_head_4','success_head_desc_4','success_head_5','success_head_desc_5','success_head_6','success_head_desc_6'];
+    protected $primaryKey = 'id';
+
 }
--- database/migrations/2022_05_29_201630_create_homes.php
+++ database/migrations/2022_05_29_201630_create_homes.php
@@ -13,10 +13,56 @@ class CreateHomes extends Migration
      */
     public function up()
     {
+
         Schema::create('homes', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('home_title')->nullable();
-            $table->text('services_content');
+            $table->string('home_title')->nullable();
+            $table->string('home_title_desc')->nullable();
+            $table->string('slider_text_1')->nullable();
+            $table->string('slider_text_2')->nullable();
+            $table->string('slider_text_3')->nullable();
+            $table->text('services_heading')->nullable();
+            $table->text('services_desc')->nullable();
+            $table->string('services_icon_1')->nullable();
+            $table->string('services_heading_1')->nullable();
+            $table->string('services_desc_1')->nullable();
+            $table->string('services_icon_2')->nullable();
+            $table->string('services_heading_2')->nullable();
+            $table->string('services_desc_2')->nullable();
+            $table->string('services_icon_3')->nullable();
+            $table->string('services_heading_3')->nullable();
+            $table->string('services_desc_3')->nullable();
+            $table->string('services_icon_4')->nullable();
+            $table->string('services_heading_4')->nullable();
+            $table->string('services_desc_4')->nullable();
+            $table->string('services_icon_5')->nullable();
+            $table->string('services_heading_5')->nullable();
+            $table->string('services_desc_5')->nullable();
+            $table->string('services_icon_6')->nullable();
+            $table->string('services_heading_6')->nullable();
+            $table->string('services_desc_6')->nullable();
+            $table->string('company_heading')->nullable();
+            $table->string('company_heading_desc')->nullable();
+            $table->string('company_image')->nullable();
+            $table->string('company_desc')->nullable();
+            $table->string('company_item_1')->nullable();
+            $table->string('company_item_per_1')->nullable();
+            $table->string('company_item_2')->nullable();
+            $table->string('company_item_per_2')->nullable();
+            $table->string('success_heading')->nullable();
+            $table->string('success_heading_desc')->nullable();
+            $table->string('success_head_1')->nullable();
+            $table->string('success_head_desc_1')->nullable();
+            $table->string('success_head_2')->nullable();
+            $table->string('success_head_desc_2')->nullable();
+            $table->string('success_head_3')->nullable();
+            $table->string('success_head_desc_3')->nullable();
+            $table->string('success_head_4')->nullable();
+            $table->string('success_head_desc_4')->nullable();
+            $table->string('success_head_5')->nullable();
+            $table->string('success_head_desc_5')->nullable();
+            $table->string('success_head_6')->nullable();
+            $table->string('success_head_desc_6')->nullable();
             $table->timestamps();
         });
     }
